08e97e349cb3602aacca995dfe369b3a51fc8e1c
Kwanyeob <52995390+Kwanyeob@users.noreply.github.com>
2020-06-01 15:12:50 +0900

    Create empty.php

---
+++ pages/empty.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--Todo : Complete the meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>QianLi</title>
+    <link rel="icon" href="https://getbootstrap.com/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <!-- Bootstrap core CSS-->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="../css/styles.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <header>
+
+
+<nav class="navbar navbar-dark navbar-expand-md bg-dark justify-content-center">
+        <a href="/" class="navbar-brand d-flex w-50 mr-auto">QianLi</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+            <ul class="navbar-nav w-100 justify-content-center">
+                    <li class="nav-item">
+                            <a class="nav-link" href="#">Stencils</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">iThor</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">iCopy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PCB Cam</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                             Prying tools
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                 Removers
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                        </li>
+            </ul>
+            <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+                <li class="nav-item">
+                        <a class="nav-link" href="#">All products</a>
+                 </li>
+                 <li class="nav-item">
+                         <a class="nav-link" href="#">Downloads</a>
+                 </li>
+            </ul>
+        </div>
+    </nav>
+    </header>
+
+    <main>
+    <!-- Put your content here-->
+        <div class="jumbotron">
+            <div class="container">
+                <!-- Put your content here-->
+                
+            </div>
+        </div>
+    </main>
+
+    <div class="footer-dark">
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>Vending</h3>
+                            <ul>
+                                <li><a href="#">Where to buy</a></li>
+                                <li><a href="#">Distributors</a></li>
+                                <li><a href="#">Alibaba store</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>Contact</h3>
+                            <ul>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">E-mail us</a></li>
+                                <li><a href="#">Address</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6 col-md-3 item">
+                            <h3>About us</h3>
+                            <ul>
+                                <li><a href="#">Stories</a></li>
+                                <li><a href="#">Team</a></li>
+                                <li><a href="#">Career</a></li>
+                            </ul>
+                        </div>
+                        <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                    </div>
+                    <p class="copyright">QianLi ToolPlus Co., Ltd. © 2019</p>
+                </div>
+            </footer>
+        </div>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
